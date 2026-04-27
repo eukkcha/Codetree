1d1204bf0077bcb604933c2581380ddfe70ad26a
JeonYuchan <eukkchan03@gmail.com>
2026-04-27 19:10:12 +0900

    Title: 사칙연산 연습, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+    int ans1 = c/8;
+    int ans2 = c%8;
+    cout << ans1 << "\n";
+    cout << ans2;
+    return 0;
+}
