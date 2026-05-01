515e9a5bd58b9f87b00c685e26d2e8a0ad50100e
JeonYuchan <eukkchan03@gmail.com>
2026-05-01 15:59:16 +0900

    [Passed] 친근하지 않은 수 (8ms, 0MB)

---
+++ trail0/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+        if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
