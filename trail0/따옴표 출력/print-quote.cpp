69950ff205ea2c0b62d65953ccb70f51bbc5c680
JeonYuchan <eukkchan03@gmail.com>
2026-05-01 15:14:37 +0900

    [Passed] 따옴표 출력 (8ms, 0MB)

---
+++ trail0/따옴표 출력/print-quote.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str = "He says \"It\'s a really simple sentence\".";
+    cout << str;
+
+    return 0;
+}
