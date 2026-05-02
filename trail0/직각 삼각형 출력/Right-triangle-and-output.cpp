bc56d56203803b7bf6b78b83e1bad106afbdc04d
JeonYuchan <eukkchan03@gmail.com>
2026-05-02 14:05:38 +0900

    [Passed] 직각 삼각형 출력 (8ms, 0MB)

---
+++ trail0/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < (i + 1) * 2 - 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
