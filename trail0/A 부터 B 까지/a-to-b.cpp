4f7c2da98b9985383cf54937ccd1087061da3e83
JeonYuchan <eukkchan03@gmail.com>
2026-05-02 12:21:31 +0900

    [Passed] A 부터 B 까지 (8ms, 0MB)

---
+++ trail0/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+
+    while (A <= B) {
+        cout << A << " ";
+        if (A % 2 == 1) {
+            A *= 2;
+        }
+        else {
+            A += 3;
+        }
+    }
+
+    return 0;
+}
