3ab69271c5f7c90b4888ab21cc0b1a3aeffe7c2d
JeonYuchan <eukkchan03@gmail.com>
2026-05-02 11:50:26 +0900

    [Passed] A부터 B까지 짝수의 합 (8ms, 0MB)

---
+++ trail0/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    
+    int sum = 0;
+    for (int i = A; i < B + 1; i++) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
